73705b618c9aa541b603fcabf734225c9fcdcbab
Manuel Burdach <manuelburdach@outlook.de>
2023-02-28 12:27:37 +0100

    Upload

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,6 +1,14 @@
 //CSS import
 import "./Blog.css";
 
+//Image import
+import tomons from "../../Assets/images/d2.png";
+import uconAcrobatics from "../../Assets/images/u6.png";
+import retrospec from "../../Assets/images/u10.png";
+import fellow from "../../Assets/images/u9.png";
+import candylabToys from "../../Assets/images/h17.png";
+import taschen from "../../Assets/images/c3.png";
+
 const Blog = () => {
   return (
     <section id="blog">
@@ -9,106 +17,124 @@ const Blog = () => {
       </div>
       <article>
         <h2>
-          Blogarticle <span> 20.02.2023 - 14:44</span>
+          Blog: new article (Tomons) <span> 20.02.2023 - 14:44</span>
         </h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, animi, atque, soluta
-          illo voluptas odit fugiat neque dolor tempore quas non cupiditate. Perferendis neque vero
-          harum amet quis. Aliquam beatae tempora eveniet sit aliquid cupiditate repellendus
-          quisquam totam facere alias, similique dignissimos? Laboriosam numquam voluptas explicabo,
-          amet animi eaque dicta ab non ex voluptatum. A accusantium autem facere consectetur
-          deleniti! Eum amet fugit magnam tempora nisi soluta fuga dolorem dicta accusamus incidunt
-          nobis dignissimos quis, placeat iure quas nam corporis adipisci molestias deserunt itaque
-          ut blanditiis enim provident? Distinctio minima nostrum reiciendis perferendis officia,
-          blanditiis animi, et aliquam veniam ut eaque voluptatibus facilis sint magni veritatis,
-          alias corporis? Similique explicabo, veniam tempore quae, assumenda esse rerum, odit quod
-          a sapiente eum dolorum. Quia accusamus, nulla numquam ullam totam provident illo adipisci
-          fugit assumenda a eos nostrum suscipit maxime, architecto esse cum libero culpa? Facere
-          placeat libero qui dignissimos nostrum voluptates necessitatibus veniam dolores cupiditate
-          dolorum a sequi repellat magnam, reiciendis nesciunt, aliquid quidem! Quod, autem atque
-          omnis, impedit incidunt sit voluptatibus vero quae veritatis assumenda, suscipit sequi
-          similique. Quam, cumque? Culpa doloremque itaque velit rem expedita! Eius repellat
-          aspernatur ex qui maiores nostrum in fuga, doloremque nulla distinctio corporis
-          temporibus.
-        </p>
+        <div>
+          <img src={tomons} alt="Tomons" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, animi, atque, soluta
+            illo voluptas odit fugiat neque dolor tempore quas non cupiditate. Perferendis neque
+            vero harum amet quis. Aliquam beatae tempora eveniet sit aliquid cupiditate repellendus
+            quisquam totam facere alias, similique dignissimos? Laboriosam numquam voluptas
+            explicabo, amet animi eaque dicta ab non ex voluptatum. A accusantium autem facere
+            consectetur deleniti! Eum amet fugit magnam tempora nisi soluta fuga dolorem dicta
+            accusamus incidunt nobis dignissimos quis, placeat iure quas nam corporis adipisci
+            molestias deserunt itaque ut blanditiis enim provident? Distinctio minima nostrum
+            reiciendis perferendis officia, blanditiis animi, et aliquam veniam ut eaque
+            voluptatibus facilis sint magni veritatis, alias corporis? Similique explicabo, veniam
+            tempore quae, assumenda esse rerum, odit quod a sapiente eum dolorum. Quia accusamus,
+            nulla numquam ullam totam provident illo adipisci fugit assumenda a eos nostrum suscipit
+            maxime, architecto esse cum libero culpa? Facere placeat libero qui dignissimos nostrum
+            voluptates necessitatibus veniam dolores cupiditate dolorum a sequi repellat magnam,
+            reiciendis nesciunt, aliquid quidem! Quod, autem atque omnis, impedit incidunt sit
+            voluptatibus vero quae veritatis assumenda, suscipit sequi similique. Quam, cumque?
+            Culpa doloremque itaque velit rem expedita! Eius repellat aspernatur ex qui maiores
+            nostrum in fuga, doloremque nulla distinctio corporis temporibus.
+          </p>
+        </div>
       </article>
 
       <article>
         <h2>
-          Blogarticle <span> 12.01.2023 - 12:01</span>
+          Blog: new article (Ucon Acrobatics) <span> 12.01.2023 - 12:01</span>
         </h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum quibusdam nostrum
-          reiciendis deserunt qui quod, placeat rem veritatis nisi cumque perferendis hic inventore
-          facilis fuga. Error quas suscipit quibusdam ut ad, provident maxime, vitae quaerat
-          praesentium nulla quasi exercitationem. Delectus dolores vero cumque dolorum tempore
-          reiciendis? Mollitia maiores totam aspernatur.
-        </p>
+        <div>
+          <img src={uconAcrobatics} alt="uconAcrobatics" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum quibusdam nostrum
+            reiciendis deserunt qui quod, placeat rem veritatis nisi cumque perferendis hic
+            inventore facilis fuga. Error quas suscipit quibusdam ut ad, provident maxime, vitae
+            quaerat praesentium nulla quasi exercitationem. Delectus dolores vero cumque dolorum
+            tempore reiciendis? Mollitia maiores totam aspernatur.
+          </p>
+        </div>
       </article>
       <div>
         <p>2022</p>
       </div>
       <article>
         <h2>
-          Blogarticle <span> 01.11.2022 - 08:23</span>
+          Blog: new article (Retrospec) <span> 01.11.2022 - 08:23</span>
         </h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error consectetur saepe quisquam
-          quidem quas facere?
-        </p>
+        <div>
+          <img src={retrospec} alt="retrospec" />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error consectetur saepe
+            quisquam quidem quas facere?
+          </p>
+        </div>
       </article>
       <article>
         <h2>
-          Blogarticle <span> 15.07.2022 - 11:22</span>
+          Blog: new article (Fellow) <span> 15.07.2022 - 11:22</span>
         </h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum neque rem laboriosam id
-          labore ipsum enim sed vitae, dolor magnam ipsam perspiciatis ea deleniti eos. Ad modi
-          rerum consequatur asperiores quidem tempora quo, quisquam ullam sunt eligendi eaque
-          doloremque, consequuntur expedita ducimus? Officia quod obcaecati quia enim incidunt
-          provident sint, unde doloremque, dignissimos ipsam asperiores nemo ratione. Pariatur
-          consectetur eveniet minus quaerat officiis provident ad distinctio, asperiores cumque
-          incidunt iure soluta dolore modi vero ab voluptatibus error deserunt voluptatem
-          consequuntur a reprehenderit quae aspernatur? Earum pariatur quam, cumque eaque, dicta,
-          nesciunt ducimus quos et officia libero magni excepturi eos similique!
-        </p>
+        <div>
+          <img src={fellow} alt="fellow" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum neque rem laboriosam id
+            labore ipsum enim sed vitae, dolor magnam ipsam perspiciatis ea deleniti eos. Ad modi
+            rerum consequatur asperiores quidem tempora quo, quisquam ullam sunt eligendi eaque
+            doloremque, consequuntur expedita ducimus? Officia quod obcaecati quia enim incidunt
+            provident sint, unde doloremque, dignissimos ipsam asperiores nemo ratione. Pariatur
+            consectetur eveniet minus quaerat officiis provident ad distinctio, asperiores cumque
+            incidunt iure soluta dolore modi vero ab voluptatibus error deserunt voluptatem
+            consequuntur a reprehenderit quae aspernatur? Earum pariatur quam, cumque eaque, dicta,
+            nesciunt ducimus quos et officia libero magni excepturi eos similique!
+          </p>
+        </div>
       </article>
       <article>
         <h2>
-          Blogarticle <span> 01.02.2022 - 16:23</span>
+          Blog: new article (Candylab Toys) <span> 01.02.2022 - 16:23</span>
         </h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam harum pariatur impedit
-          nihil ipsam vel minus esse hic, temporibus ipsum libero. Nam dolores soluta facere
-          distinctio maiores pariatur iusto possimus veritatis exercitationem similique! Error
-          consectetur vel iusto hic natus? Sint, nisi odio. Veniam fugiat quam laboriosam nesciunt
-          consectetur accusamus quisquam aperiam harum obcaecati expedita magnam perferendis odit,
-          error excepturi distinctio velit itaque, iure incidunt similique aliquam facilis dolorum
-          laudantium a. Expedita quo magnam dicta optio dolore eum eveniet recusandae ratione
-          architecto et? Obcaecati quia assumenda quas deserunt fuga nemo magnam labore cupiditate
-          aliquam, amet tempore sit, quisquam nostrum aperiam et quasi blanditiis exercitationem?
-          Unde a asperiores commodi illo, laborum ut porro exercitationem pariatur saepe eligendi
-          dolore velit quasi tempore odio iste quos eius harum eveniet. Dolores, nostrum sapiente?
-          Quos nisi tempore, ea velit nulla libero ipsum perspiciatis asperiores rem qui soluta quia
-          adipisci officiis excepturi quidem debitis neque. Molestias dolore voluptas quod pariatur,
-          inventore doloremque suscipit molestiae excepturi blanditiis numquam vitae distinctio
-          reiciendis aperiam perferendis culpa earum nulla eius consectetur deserunt. Quos quas enim
-          itaque, veritatis quae reprehenderit sequi illum repellendus dolorem vel, exercitationem
-          deserunt optio vitae. Sit, magnam iure sapiente, expedita itaque saepe nulla, quisquam
-          aperiam voluptatum ducimus ab?
-        </p>
+        <div>
+          <img src={candylabToys} alt="candylabToys" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam harum pariatur
+            impedit nihil ipsam vel minus esse hic, temporibus ipsum libero. Nam dolores soluta
+            facere distinctio maiores pariatur iusto possimus veritatis exercitationem similique!
+            Error consectetur vel iusto hic natus? Sint, nisi odio. Veniam fugiat quam laboriosam
+            nesciunt consectetur accusamus quisquam aperiam harum obcaecati expedita magnam
+            perferendis odit, error excepturi distinctio velit itaque, iure incidunt similique
+            aliquam facilis dolorum laudantium a. Expedita quo magnam dicta optio dolore eum eveniet
+            recusandae ratione architecto et? Obcaecati quia assumenda quas deserunt fuga nemo
+            magnam labore cupiditate aliquam, amet tempore sit, quisquam nostrum aperiam et quasi
+            blanditiis exercitationem? Unde a asperiores commodi illo, laborum ut porro
+            exercitationem pariatur saepe eligendi dolore velit quasi tempore odio iste quos eius
+            harum eveniet. Dolores, nostrum sapiente? Quos nisi tempore, ea velit nulla libero ipsum
+            perspiciatis asperiores rem qui soluta quia adipisci officiis excepturi quidem debitis
+            neque. Molestias dolore voluptas quod pariatur, inventore doloremque suscipit molestiae
+            excepturi blanditiis numquam vitae distinctio reiciendis aperiam perferendis culpa earum
+            nulla eius consectetur deserunt. Quos quas enim itaque, veritatis quae reprehenderit
+            sequi illum repellendus dolorem vel, exercitationem deserunt optio vitae. Sit, magnam
+            iure sapiente, expedita itaque saepe nulla, quisquam aperiam voluptatum ducimus ab?
+          </p>
+        </div>
       </article>
       <div>
         <p>2021</p>
       </div>
       <article>
         <h2>
-          Blogarticle <span> 01.11.2021 - 12:00</span>
+          Blog: new article (Taschen) <span> 01.11.2021 - 12:00</span>
         </h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error consectetur saepe quisquam
-          quidem quas facere?
-        </p>
+        <div>
+          <img src={taschen} alt="taschen" />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error consectetur saepe
+            quisquam quidem quas facere?
+          </p>
+        </div>
       </article>
     </section>
   );
